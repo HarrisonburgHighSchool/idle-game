769acbc5fceb0e73605d16a89f2eef1bf851a117
Jon <jstapleton463@harrisonburg.k12.va.us>
2019-09-16 09:54:32 -0400

    Added Comment

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var score = 0;
 
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress);
+createButton('Button', btnPress); // Make a button
 changeTitle('Clicker Game');
 
 setInterval(btnPress, 1000);
