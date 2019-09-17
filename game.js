5ca4ec3da9bbfd79db50bfb224fba5563343d88a
Jon <jstapleton463@harrisonburg.k12.va.us>
2019-09-17 09:11:37 -0400

    added a button

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ var score = 0;
 postToPage(score); // Print to the page
 
 createButton('Button', btnPress); // Make a button
+createButton('hello');
 changeTitle('Clicker Game');
 createText('Hello');
 
