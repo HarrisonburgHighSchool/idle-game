5453ea7ae9013e87890485a96cf32c9c1f934568
Jon <jstapleton463@harrisonburg.k12.va.us>
2019-09-18 10:53:08 -0400

    Added fades to element transitions

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ var score = 1; // Create a variable to store the score in
 
 var title = new Title('Welcome to the Game'); // Create a title element
 var button = new Button('hello', btnPress);   // Create a button element
+var hidden = new Button('Boo!');
+hidden.hide();
 var scr = new Text(score);                    // Create a text element
 
 setInterval(btnPress, 1000);   // Set up a loop
@@ -9,4 +11,10 @@ setInterval(btnPress, 1000);   // Set up a loop
 function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
+  if(score > 10) {
+    hidden.show();
+  }
+  if(score > 15) {
+    hidden.hide(500);
+  }
 }
