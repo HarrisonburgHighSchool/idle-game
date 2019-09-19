32827d9e7a0f6c13066c9abd88ee83dc9bdf98d7
Eric Tolliver <eitoll21@harrisonburg.k12.va.us>
2019-09-19 09:44:52 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,17 +1,15 @@
-var score = 1; // Create a variable to store the score in
+var score = 0; // Create a variable to store the score in
 
 var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
+//var button = new Button('hello', btnPress);   // Create a button element
 var scr = new Text(score);                    // Create a text element
 
 
-createButton('higher', btnPress);
-createButton('lower', btnPress2);
+var button1 = new Button('higher', btnPress);
+var button2 = new Button('lower', btnPress2);
 
-changeTitle('Clicker Game');
+//changeTitle('Clicker Game');
 createText('Hello');
-
-setInterval(btnPress, 100);
 
 function btnPress() {
   score++;         // Increase the score
@@ -20,5 +18,14 @@ function btnPress() {
 
 function btnPress2() {
   score--;
-  postToPage(score);
+  scr.edit(score); // Update the page with the new score
+}
+
+setIncrease(scoreIcrease, 1000);// Run the "scoreIncrease" function once every
+function scoreIncrease() {
+  score++;                 //Increase the score
+}
+
+if(score > 10){
+  button.show(); //If the score reaches 100, show the button!
 }
