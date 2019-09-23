ad757e24e5afa39be0ce468557f1b1ec428d27ad
Eric Tolliver <eitoll21@harrisonburg.k12.va.us>
2019-09-23 09:54:07 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,26 +1,21 @@
 var score = 0; // Create a variable to store the score in
-
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
-hidden.hide();
+var title = new Title('Space Attack'); // Create a title element
 var scr = new Text(score);                    // Create a text element
 
-
-var button1 = new Button('higher', btnPress);
+var button1 = new Button('higher', btnPress1);
 var button2 = new Button('lower', btnPress2);
 
-//changeTitle('Clicker Game');
-createText('Hello');
 
-function btnPress() {
+
+
+function btnPress1() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
-  if(score > 10) {
+  if(score > 1000) {
     hidden.show();
   }
-  if(score > 15) {
-    hidden.hide(500);
+
+  if(score > 15) {  
   }
 }
 
@@ -33,6 +28,6 @@ function scoreIncrease() {
   score++;                 //Increase the score
 }
 
-if(score > 10){
+if(score > 1000){
   button.show(); //If the score reaches 100, show the button!
 }
