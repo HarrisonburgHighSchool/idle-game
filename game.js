593b086239229c1d0bcf8c51479f39420d55521c
Eric Tolliver <eitoll21@harrisonburg.k12.va.us>
2019-09-26 09:54:01 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,13 +2,31 @@ var score = 0; // Create a variable to store the score in
 var title = new Title('Space Attack'); // Create a title element
 var scr = new Text(score);                    // Create a text element
 
-var coinButton = new Button('coins', btnPress1);
-var button2 = new Button('lower', btnPress2);
+var button = new Button('Text')
+var coinButton = new Button('Coins', btnPress1);
+var button2 = new Button('Weapons', btnPress2);
 
 var text1 = new Text('weapons');  // Create some text to go in the columns
 var text2 = new Text('I will go in column 2');
 
+var section = new Section();
+var text =new Text('Here is some text');
 
+section.add(text);
+section.addBorder('green');
+
+var rowSection = new Section('row');
+var column1 = new Section('col')
+var column2 = new Section('col')
+
+rowSection.add(column1);
+rowSection.add(column2);
+section.style('height', '20rem')
+
+column1.add(text1);
+column2.add(text2);
+
+column1.add(section)
 setInterval(btnPress1, 200); // Run the "createButton" function once every 1000 milliseconds (once every second)
 
 function coinButton() {
@@ -28,8 +46,13 @@ function btnPress1() {
 }
 
 function btnPress2() {
-  
-  scr.edit(score); // Update the page with the new score
+  var section = new Section();
+var text = new Text('Here is some text');
+
+section.add(text);
+section.addBorder();
+
+  scr.edit(score); // Update the page with list of weapons
 }
 
 function scoreIncrease() {
